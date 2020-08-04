9bc3584960e9f8b550f896191cc4844d76b0459b
zhengziqiang <1174986943@qq.com>
2020-08-04 15:02:28 +0800

    change the epoch number

--- scripts/train_flower.sh
+++ scripts/train_flower.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python main.py --phase train --epoch 120 --dataset_dir flower --checkpoint_dir ./check/flower --sample_dir ./check/flower/sample --gpu 0 --L1_lambda 10
+python main.py --phase train --epoch 40 --dataset_dir flower --checkpoint_dir ./check/flower --sample_dir ./check/flower/sample --gpu 0 --L1_lambda 10
